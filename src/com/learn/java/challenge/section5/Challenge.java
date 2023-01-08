1c6550cded0309a4140cad694db186ee3acc2f75
vimaljeet <jeetvimal98singh@gmail.com>
2023-01-08 11:34:28 +0530

    Section 5 CE10 added

--- src/com/learn/java/challenge/section5/Challenge.java
+++ src/com/learn/java/challenge/section5/Challenge.java
@@ -52,4 +52,15 @@ public class Challenge {
 		}
 		return false;
 	}
+	
+	public static double area(double radius) {
+        if(radius < 0) return -1.0;
+        
+        return Math.PI*radius*radius;
+    }
+    public static double area(double x, double y) {
+        if(x < 0 || y < 0) return -1.0;
+        
+        return x*y;
+    }
 }
